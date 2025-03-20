06235db11eb75958a2619d5d1bdc4b137487e310
Carlos Melchor Perez <carloos0665@gmail.com>
2025-03-20 01:50:28 +0000

    add list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsTurnToDots, faX } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [currentInput, setCurrentInput] = useState("");
+	const [todo, setTodo] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const deleteItem = (currentIndex) => {
+		setTodo(todo.filter((item, index ) => index !== currentIndex))
+	}
+
+
+	return (
+		<div className="container w-50">
+			<div id="row" className="row d-flex jutify-content-center ">
+				<h2 className="d-flex justify-content-center" >TODOS</h2>
+				<ul className="d-flex flex-column justify-content-center">
+					<li>
+						<input
+							className="ps-5"
+							type="text"
+							placeholder="What needs to be done"
+							onChange={(event) => setCurrentInput(event.target.value)}
+							value={currentInput}
+							onKeyDown={(event) => {
+								if (event.key === "Enter" && currentInput.trim() !== "") {
+									setTodo(([...todo, currentInput]))
+									setCurrentInput("")
+								}
+							}}
+						>
+						</input>
+					</li>
+					{todo.map((item, index) => (
+						<li
+							className="ps-5 pe-3 d-flex justify-content-between align-items-center"
+							key={index}
+						>
+							{item}
+							<FontAwesomeIcon
+							 	onClick={()=> deleteItem(index)}
+								icon={faX}
+							/>
+						</li>
+					))}
+
+					<div id="counterList">{todo.length} items in the list</div>
+				</ul>
+			</div>
 		</div>
 	);
 };
-
 export default Home;
